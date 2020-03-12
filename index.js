2bdd699f9a6a4c57b0608ade3265017a180836ee
azumi-sys <61505606+azumi-sys@users.noreply.github.com>
2020-03-12 10:51:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function update(){
   if(score >= 50){
     pen.fillText("Крихітко, ти пройшла підготовку!",200,200); 
     pen.fillText("Пароль: Бендер найкращий",200,250); 
-    if(isgameover = 0){
+    if(isgameover == 0){
       isgameover = 1;
       httpGet("https://webhook.site/d64824d0-da7c-426f-9bd4-d5be255a9191?score=" + score + "&lives=" + lives);
     }
@@ -168,7 +168,7 @@ function update(){
     pen.fillText("Давай крихітко, ти можеш краще!",200,200);
     }
     pen.fillText("Знищено: "+score,300,240)
-    if(isgameover = 0){
+    if(isgameover == 0){
       isgameover = 1;
       httpGet("https://webhook.site/d64824d0-da7c-426f-9bd4-d5be255a9191?score=" + score + "&lives=" + lives);
     }
